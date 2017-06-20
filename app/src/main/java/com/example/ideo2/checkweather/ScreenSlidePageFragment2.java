87de7e41cc68bdbb317add046667409c9761c133
Leone <leone0203@gmail.com>
2017-06-20 14:02:07 +0200

    screen slide pagers  added

---
+++ app/src/main/java/com/example/ideo2/checkweather/ScreenSlidePageFragment2.java
@@ -0,0 +1,78 @@
+package com.example.ideo2.checkweather;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ideo2 on 23.09.2016.
+ */
+
+public class ScreenSlidePageFragment2 extends android.support.v4.app.Fragment {
+
+    TextView cityNameL;
+    TextView countryCodeL;
+    TextView dayOfWeek;
+    TextView weatherDesc;
+    TextView mornT;
+    TextView dayT;
+    TextView nightT;
+    TextView pressureL;
+    TextView humidityL;
+    RelativeLayout parentLayout;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen, container, false);
+
+        //variables from activity3
+        String cityName = WeatherActivity.cityName;
+        String countryCode = WeatherActivity.countryCode;
+        ArrayList<Double> tempMornArray = WeatherActivity.tempMornArray;
+        ArrayList<Double> tempDayArray = WeatherActivity.tempDayArray;
+        ArrayList<Double> tempNightArray = WeatherActivity.tempNightArray;
+        ArrayList<Double> humidity = WeatherActivity.humidity;
+        ArrayList<Double> pressure = WeatherActivity.pressure;
+        ArrayList<String> mainArray = WeatherActivity.mainArray;
+        ArrayList<String> descriptionArray = WeatherActivity.descriptionArray;
+
+        //layout elements
+        cityNameL = (TextView) rootView.findViewById(R.id.cityId);
+        countryCodeL = (TextView) rootView.findViewById(R.id.countryCodeId);
+        dayOfWeek  = (TextView) rootView.findViewById(R.id.dayScroll);
+        weatherDesc  = (TextView) rootView.findViewById(R.id.weatherScroll);
+        mornT =  (TextView) rootView.findViewById(R.id.morningScroll);
+        dayT =  (TextView) rootView.findViewById(R.id.tempDayScroll);
+        nightT =  (TextView) rootView.findViewById(R.id.nightScroll);
+        pressureL =  (TextView) rootView.findViewById(R.id.pressureScroll);
+        humidityL = (TextView) rootView.findViewById(R.id.humidityScroll);
+        parentLayout = (RelativeLayout) rootView.findViewById(R.id.parentLayout);
+
+        //set data
+        cityNameL.setText(cityName);
+        countryCodeL.setText(countryCode);
+        dayOfWeek.setText("Tommorow");
+        weatherDesc.setText(descriptionArray.get(1));
+        mornT.setText("Morning: "+(double)Math.round((tempMornArray.get(1)-273.15)*10d)/10d+" \u00B0C");
+        dayT.setText((double)Math.round((tempDayArray.get(1)-273.15)*10d)/10d+" \u00B0C");
+        nightT.setText("Night: "+(double)Math.round((tempNightArray.get(1)-273.15)*10d)/10d+" \u00B0C");
+        pressureL.setText("Pressure: "+pressure.get(1)+" hPa");
+        humidityL.setText("Humidity: "+humidity.get(1)+" %");
+
+        if (mainArray.get(1).matches("Clear")) parentLayout.setBackgroundResource(R.drawable.clear_background);
+        if (mainArray.get(1).matches("Rain")) parentLayout.setBackgroundResource(R.drawable.rain_background);
+        if (mainArray.get(1).matches("Clouds")) parentLayout.setBackgroundResource(R.drawable.clouds_background);
+        if (mainArray.get(1).matches("Snow")) parentLayout.setBackgroundResource(R.drawable.snow_background);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/ideo2/checkweather/ScreenSlidePageFragment6.java
@@ -0,0 +1,126 @@
+package com.example.ideo2.checkweather;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by ideo2 on 23.09.2016.
+ */
+
+public class ScreenSlidePageFragment6 extends android.support.v4.app.Fragment {
+
+
+
+    TextView cityNameL;
+    TextView countryCodeL;
+    TextView dayOfWeek;
+    TextView weatherDesc;
+    TextView mornT;
+    TextView dayT;
+    TextView nightT;
+    TextView pressureL;
+    TextView humidityL;
+
+    RelativeLayout parentLayout;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen, container, false);
+
+
+        //variables from activity3
+        String cityName = WeatherActivity.cityName;
+        String countryCode = WeatherActivity.countryCode;
+        ArrayList<Double> tempMornArray = WeatherActivity.tempMornArray;
+        ArrayList<Double> tempDayArray = WeatherActivity.tempDayArray;
+        ArrayList<Double> tempNightArray = WeatherActivity.tempNightArray;
+        ArrayList<Double> humidity = WeatherActivity.humidity;
+        ArrayList<Double> pressure = WeatherActivity.pressure;
+        ArrayList<String> mainArray = WeatherActivity.mainArray;
+        ArrayList<String> descriptionArray = WeatherActivity.descriptionArray;
+
+        //layout elements
+        cityNameL = (TextView) rootView.findViewById(R.id.cityId);
+        countryCodeL = (TextView) rootView.findViewById(R.id.countryCodeId);
+        dayOfWeek  = (TextView) rootView.findViewById(R.id.dayScroll);
+        weatherDesc  = (TextView) rootView.findViewById(R.id.weatherScroll);
+        mornT =  (TextView) rootView.findViewById(R.id.morningScroll);
+        dayT =  (TextView) rootView.findViewById(R.id.tempDayScroll);
+        nightT =  (TextView) rootView.findViewById(R.id.nightScroll);
+        pressureL =  (TextView) rootView.findViewById(R.id.pressureScroll);
+        humidityL = (TextView) rootView.findViewById(R.id.humidityScroll);
+        parentLayout = (RelativeLayout) rootView.findViewById(R.id.parentLayout);
+
+        //set data
+        cityNameL.setText(cityName);
+        countryCodeL.setText(countryCode);
+        weatherDesc.setText(descriptionArray.get(5));
+        mornT.setText("Morning: "+(double)Math.round((tempMornArray.get(5)-273.15)*10d)/10d+" \u00B0C");
+        dayT.setText((double)Math.round((tempDayArray.get(5)-273.15)*10d)/10d+" \u00B0C");
+        nightT.setText("Night: "+(double)Math.round((tempNightArray.get(5)-273.15)*10d)/10d+" \u00B0C");
+        pressureL.setText("Pressure: "+pressure.get(5)+" hPa");
+        humidityL.setText("Humidity: "+humidity.get(5)+" %");
+
+
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch (day) {
+
+            case Calendar.MONDAY: {
+                dayOfWeek.setText("Saturday");
+                break;
+            }
+            case Calendar.TUESDAY: {
+                dayOfWeek.setText("Sunday");
+                break;
+            }
+            case Calendar.WEDNESDAY: {
+                dayOfWeek.setText("Monday");
+                break;
+            }
+            case Calendar.THURSDAY: {
+                dayOfWeek.setText("Tuesday");
+                break;
+            }
+            case Calendar.FRIDAY: {
+                dayOfWeek.setText("Wednesday");
+                break;
+            }
+            case Calendar.SATURDAY: {
+                dayOfWeek.setText("Thursday");
+                break;
+            }
+            case Calendar.SUNDAY: {
+                dayOfWeek.setText("Friday");
+                break;
+            }
+            default: {
+                dayOfWeek.setText("Day6");
+                break;
+            }
+
+        }
+
+
+        if (mainArray.get(5).matches("Clear")) parentLayout.setBackgroundResource(R.drawable.clear_background);
+        if (mainArray.get(5).matches("Rain")) parentLayout.setBackgroundResource(R.drawable.rain_background);
+        if (mainArray.get(5).matches("Clouds")) parentLayout.setBackgroundResource(R.drawable.clouds_background);
+        if (mainArray.get(5).matches("Snow")) parentLayout.setBackgroundResource(R.drawable.snow_background);
+
+        return rootView;
+    }
+}
